07b43032ceed1bb7447d67b278b57f9c66a93dbb
Александр Бебякин <lorpaxx@yandex.ru>
2022-06-01 16:36:30 +0300

    check filters

--- backend/foodgram_project/api/filters.py
+++ backend/foodgram_project/api/filters.py
@@ -1,5 +1,4 @@
 from django_filters import FilterSet, rest_framework
-# from recipes.models import Recipe
 from tags.models import Tag
 
 
@@ -12,14 +11,16 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-
+    '''
+    Класс RecipeFilter.
+    '''
     author = rest_framework.CharFilter(
         field_name='author__id',
     )
-    is_favorited = rest_framework.CharFilter(method='is_favorited_filter')
+    is_favorited = rest_framework.CharFilter(method='get_is_favorited_filter')
 
     is_in_shopping_cart = rest_framework.CharFilter(
-        method='is_in_shopping_cart_filter'
+        method='get_is_in_shopping_cart_filter'
     )
 
     tags = rest_framework.ModelMultipleChoiceFilter(
@@ -29,7 +30,11 @@ class RecipeFilter(FilterSet):
         conjoined=True,
     )
 
-    def is_favorited_filter(self, queryset, name, value):
+    def get_is_favorited_filter(self, queryset, name, value):
+        '''
+        Возвращает отфильтрованный queryset модели Recipe,
+        элементы которых есть в UserFavoriteRecipe.
+        '''
         user = getattr(self.request, 'user', None)
         if user and user.is_authenticated:
             if value == '1':
@@ -38,7 +43,11 @@ class RecipeFilter(FilterSet):
                 return queryset.exclude(in_favorite__user=user)
         return queryset
 
-    def is_in_shopping_cart_filter(self, queryset, name, value):
+    def get_is_in_shopping_cart_filter(self, queryset, name, value):
+        '''
+        Возвращает отфильтрованный queryset модели Recipe,
+        элементы которых есть в UserShoppingCart.
+        '''
         user = getattr(self.request, 'user', None)
         if user and user.is_authenticated:
             if value == '1':
