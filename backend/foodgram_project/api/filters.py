372d7dccf0e2f1fc7d5883f8c285b5ce0b3fe024
Александр Бебякин <lorpaxx@yandex.ru>
2022-06-03 10:20:21 +0300

    after 2 review 1 chech filter recipes by tags 1

--- backend/foodgram_project/api/filters.py
+++ backend/foodgram_project/api/filters.py
@@ -29,7 +29,7 @@ class RecipeFilter(FilterSet):
         queryset=Tag.objects.all(),
         field_name='tags__slug',
         to_field_name='slug',
-        lookup_type='in'
+        lookup_type='in',
         # conjoined=True,
     )
 
